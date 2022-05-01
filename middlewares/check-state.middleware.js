04b883d3f49c7941431e94cbb4ea786294302ca9
kbotova <89753557+kbotova@users.noreply.github.com>
2022-05-01 18:32:45 -0500

    Update check-state.middleware.js

--- middlewares/check-state.middleware.js
+++ middlewares/check-state.middleware.js
@@ -4,7 +4,7 @@ const checkStateMiddleware = (req, res, next) => {
 	const { state } = req.params;
 	const foundState = states.find(s => s.code === state.toUpperCase());
 
-	if (!foundState) return res.status(404).json('Invalid state abbreviation parameter');
+	if (!foundState) return res.status(404).json({message: 'Invalid state abbreviation parameter'});
 
 	res.locals.state = foundState;
 
